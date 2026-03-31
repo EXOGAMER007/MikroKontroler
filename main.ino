843e0b75155a3c0b0209ee118e4af16c4b57bc78
EXOGAMER007 <daffa8110@gmail.com>
2026-03-31 10:15:08 +0800

    test

---
+++ main.ino
@@ -0,0 +1,62 @@
+
+
+// 1. Identitas Blynk
+#define BLYNK_TEMPLATE_ID "TMPL6hMV8s4H5"
+#define BLYNK_TEMPLATE_NAME "monitor suhu esp32"
+#define BLYNK_AUTH_TOKEN "DSORNVTgYLbO4gdzuSoJTn7rSQjyM2U5"
+
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
+
+// 2. Konfigurasi WiFi & Sensor
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Wokwi-GUEST";
+char pass[] = "";
+
+#define DHT11_PIN 15 // Pin data DHT11
+// #define DHTTYPE DHT11       // Jenis sensor
+DHT dht11(DHT11_PIN, DHT11);
+
+BlynkTimer timer; // Membuat timer agar tidak membanjiri server Blynk
+
+// 3. Fungsi Kirim Data Sensor
+void kirimDataSensor()
+{
+  float h = dht11.readHumidity();
+  float t = dht11.readTemperature();
+
+  // Cek jika sensor gagal terbaca
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("Gagal membaca sensor DHT!");
+    return;
+  }
+
+  // Kirim data ke Virtual Pin Blynk
+  Blynk.virtualWrite(V0, t); // V0 untuk Suhu
+  Blynk.virtualWrite(V1, h); // V1 untuk Kelembapan
+
+  Serial.print("Suhu: ");
+  Serial.print(t);
+  Serial.print("°C | Kelembapan: ");
+  Serial.print(h);
+  Serial.println("%");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  dht11.begin();
+
+  Blynk.begin(auth, ssid, pass);
+
+  // Setel timer untuk kirim data setiap 2 detik (2000 milidetik)
+  timer.setInterval(2000L, kirimDataSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Menjalankan timer
+}
